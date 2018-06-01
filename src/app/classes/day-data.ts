116f5225eb38c95b9d4172a4e3c3b9ce2f08e951
Emmanuel Ariza <emmanuel@emmanuel.emmanuel-pc>
2018-06-01 11:26:14 -0500

    Created day-data class

---
+++ src/app/classes/day-data.ts
@@ -0,0 +1,19 @@
+export class DayData {
+	  date: string;
+      sentiment: number;
+      character: string;
+      adjectives: string[];
+      match_data: MatchData;
+    }
+
+class MatchData {
+    title: string;
+    times: string[];
+    sentiment: number[];
+    key_times: KeyTimes;
+  }
+
+class KeyTimes{
+	first_half:string;
+	second_half:string;
+}
